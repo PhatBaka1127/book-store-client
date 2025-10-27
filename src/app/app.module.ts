293f49f42b081526d1969645a1cd6b9ff131deff
Phat Nguyen <nqphat@tma.com.vn>
2025-10-27 15:40:18 +0700

    ADD: Create boook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './pages/home/home.component';
 import { CreateBookComponent } from './pages/create-book/create-book.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    CreateBookComponent
+    CreateBookComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule    
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,29 @@ import { environment } from '../../environments/environment';
 export class AuthService {
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookieService: CookieService) {}
 
   login(credentials: { email: string; password: string }): Observable<any> {
-    return this.http.post(`${this.apiUrl}/auth/login`, credentials);
+    return this.http.post<any>(`${this.apiUrl}/auth/login`, credentials).pipe(
+      tap(res => {
+        // Lưu token vào cookie
+        this.cookieService.set('token', res.accessToken, undefined, '/'); // đường dẫn '/' để dùng toàn app
+        // Lưu user info cũng có thể lưu cookie hoặc localStorage
+        this.cookieService.set('user', JSON.stringify({
+          id: res.id,
+          email: res.email,
+          role: res.role
+        }), undefined, '/');
+      })
+    );
+  }
+
+  getToken(): string {
+    return this.cookieService.get('token');
+  }
+
+  logout(): void {
+    this.cookieService.delete('token', '/');
+    this.cookieService.delete('user', '/');
   }
 }
