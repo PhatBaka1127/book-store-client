2351b93aabc7e32f102cc892dcf8ea38444083c3
Phat Nguyen <nqphat@tma.com.vn>
2025-10-29 15:14:37 +0700

    FIX: register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-register',
@@ -11,34 +12,32 @@ import { Router } from '@angular/router';
 export class RegisterComponent {
   registerForm: FormGroup;
   loading = false;
-  errorMessage = '';
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router, private toastService: ToastService) {
     this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
-      role: [1, Validators.required] // mặc định role=1
+      role: [1, Validators.required]
     });
   }
 
   onSubmit() {
     if (this.registerForm.invalid) {
-      this.errorMessage = 'Please input all information';
+      this.toastService.showMessage("Please input all field", false, 2000);
       return;
     }
 
     this.loading = true;
-    this.errorMessage = '';
 
     this.authService.register(this.registerForm.value).subscribe({
       next: (res) => {
-        console.log('✅ Register successfully', res);
-        alert('Register successfully, you can login now.');
+        console.log('Register successfully', res);
+        this.toastService.showMessage(res.message, res.result, 2000);
         this.router.navigate(['/login']);
       },
-      error: (err) => {
-        console.error('❌ Something went wrong', err);
-        this.errorMessage = err.error?.message || 'Something went wrong';
+      error: (res) => {
+        console.error('Something went wrong', res);
+        this.toastService.showMessage(res.error.message, res.error.result, 2000);
         this.loading = false;
       }
     });
